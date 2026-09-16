d37eb2a1cebf95b404244e83628f2384dee70f5a
kalaiyarasiperumal <kalaiyarasip625@gmail.com>
2026-09-16 23:42:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,9 @@ complaints = [
 @app.route("/")
 def home():
     return render_template("index.html")
-
+    @app.route("/facility")
+def facility():
+    return render_template("facility.html")
 
 # Admin dashboard
 @app.route("/admin")
